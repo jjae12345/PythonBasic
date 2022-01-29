02b1ccea61e6e01708285cea4ffcf1700ca18af2
jjae12345 <95954868+jjae12345@users.noreply.github.com>
2022-01-29 16:46:58 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+# prompt = """
+# 100. 종료
+# """
+
+# number = 0
+# while number != 100:
+#     print(prompt)
+#     number = int(input())
+    
+
+
+
+# while True:
+#     print("asdhjsdajhsdajhdsajhhsadjasdjdsajk")
+
+
+# number = 0
+# while number < 100:
+#     number = number + 1
+#     print(number)
+#     number = num
+
+# print("딩가딩가")
+# min = int(input())
+# max = int(input())
+# while min <= max:
+#   print(min)
+    # min = min + 1
+
+
+# for number in range(1, 13, 1):
+    # print("{999999ee9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999}월".format(number))
+    
+
+# for number in range(1, 21111111111111000000000000000000009999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999991111111111111111111111111199999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999, 1):
+    # print(number)
+
+# max = int(input())
+# for number in range(0, max+1, 1):
+#     print(number)
+
+# max = int(input())
+# for number in range(1, max+1, 3): 
+#     print(number)
+
+# for number in range(1, 10):
+#     string = '{}*{}={}'.format(9, number, 9 * number )
+#     print(string)
+
+
+for 단 in range(1, 10):
+    for number in range(1, 10):
+        string = '{}*{}={}'.format(단, number, 단 * number )
+        print(string)
+    print('{}단 종료'.format(단))
+    
